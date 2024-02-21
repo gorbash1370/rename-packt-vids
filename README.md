b4f0d17a34738bae989c129426432eb9ea3645c5
Gorbash1370 <144901675+gorbash1370@users.noreply.github.com>
2024-02-21 17:19:42 +0000

    First draft README.md, screenshots added to misc and neatened commentary in script.

--- README.md
+++ README.md
@@ -1,37 +1,80 @@
-# Intro
+# Intro rename_packt_vids
 This is a simple script, written in 'Novice Python', to perform the fairly niche task of renaming Packt video course files according to the module (series) and episode title. 
 
-Packt video course files are named formulaically: `video1_2.mp4` is (Series / Module 1, Episode 2). This script will rename files `S01 Mobile Devices - E02 Laptop Hardware and Components.mp4`. This use of `Sxx` and `Exx` is designed to feed into [whisper_wrapper](www.github.com/gorbash1370/whisper_wrapper) which will automatically detect `S01` and `E02` to populate the header fields in the transcript.
+Packt video course files are named formulaically: `video1_2.mp4` is (Series / Module 1, Episode 2). This script will rename files `S01 Mobile Devices - E02 Laptop Hardware and Components.mp4`. This use of `Sxx` and `Exx` is designed to feed into [whisper_wrapper](www.github.com/gorbash1370/whisper_wrapper) which will automatically detect `S01` and `E02` to populate the header fields in the transcript.  
+[![Series and Episodes](https://github.com/gorbash1370/rename_packt_vids/misc/TOC_txt_small_annot.png)](https://github.com/gorbash1370/rename_packt_vids/misc/TOC_txt_large_annot.png)
+
+[![Before and After](https://github.com/gorbash1370/rename_packt_vids/misc/before_after_smaller.png)](https://github.com/gorbash1370/rename_packt_vids/misc/before_after_large.png)
 
 
 # Program Structure
-Single file `rename_packt_videos.py`: this is all you need if you complete he user variables `path_TOC` and `path_folder` as strings directly within the file. 
-![variable_strings](https://github.com/gorbash1370/rename_packt_vids/misc/paths_string_options)
-Alternatively you can use a `config.py` file to import these variables.
-![](https://github.com/gorbash1370/rename_packt_vids/misc/imports_optional.PNG)
-No dependencies.
+* Single file `rename_packt_videos.py`.  
+
+* No dependencies.
 
 # Program Operation (high level)
+* Reads the list of series and episodes in the TOC.txt, removes the extra lines and spacing to produce a clean list of the series and episode names `TOCsplitstripped`.
+* References the `series` dictionary (user populated) and creates a list just of the series names `TOCseriesonly`.
+* Creates a list of everything else in the TOC.txt (i.e. the episode names) to create a list of episode names `TOCepisodesonly`.
+* Reads the video file names into a list (if the episode number in a series goes over 9, the order of the names will be incorrect at this point, which will be corrected later).
+* Script surrounds the digit in the video file name with a trailing _ to isolate the complete number.
+* Script adds a leading 0 to single digit numbers to ensure the alphanumeric order is correct.
+* Script replaces "video1" with `S01 series_name`,"video2" with `S02 series_name`, etc.
+* Script renames the video files on disc with these partial names, and reorders the list representation of their names (now strictly alphanumeric). The order is now a correct match for the TOC.txt entries.
+* After checking that there are at least enough TOC.txt episode names for all the files, the script writes the correct episode name to the video file name on disc.
+
 
 # Notes
-The word 'series' has been used throughout to maintain compatiblity with the [whisper_wrapper](www.github.com/gorbash1370/whisper_wrapper), however these could probably also be thought of as modules into which the course is divided. 
+* Script only works for this EXACT format of file names: `video1_1.mp4`, `video1_2.mp4`, `video1_3.mp4` etc, where `1` is the series number and the final `1`,`2`,`3` etc are the episode number. All the Packt downloaded videos I've seen follow this format.
+* The word 'series' has been used throughout to maintain compatiblity with the [whisper_wrapper](www.github.com/gorbash1370/whisper_wrapper), however these can be thought of as 'modules' into which the course is divided. 
+* There is no error handling. 
+* Recommendation: copy the originally-named video files into a separate 'backup' folder (Packt supply their video files in zip, so there will always be a backup if the zip is kept after extraction) just incase there is an error with the renaming process. 
+* Recommendation: When the rename is complete, check the filesizes of the renamed videos, in sorted order, against the ordered filesizes in 'backup' folder. Exact matches indicate files have been renamed correctly.
+    - Alternatively, comment out the lines `os.rename(os.path.join(path_folder, file), os.path.join(path_folder, new_name))`, run the script and check the print statements to verify that the new names are correct. Then, uncomment the `os.rename` lines and run the script again to rename the files.
+* There are a lot of terminal print statements, in order to verify at every stage that the correct renaming is taking place. They can always be commented out once you're happy with how the script works.
 
 
 # User setup
+1) Create a `TOC.txt` (Table of Contents) file: open `overview.html` Packt supplies, copy the module/series and filenames, paste into a text file.  
+[![Overview.html to TOC.txt](https://github.com/gorbash1370/rename_packt_vids/misc/overview_html_to_TOC_text_small.png)](https://github.com/gorbash1370/rename_packt_vids/misc/overview_html_to_TOC_text_large.png)  
+2) Check that there aren't any IDENTICAL series and episode names in the TOC.txt. If there are, the script will get confused and the files won't map correctly. If this happens, rename the episode (i.e. just put a 1 at the end of it. So, if there's a series called "Networking" and an episode (anywhere in the course) also called "Networking", you'd rename the episode to "Networking1" or "Networking_" or similar).  
+**_You do not need to do ANYTHING to the formatting. Don't reorder any lines, don't move the series names around, don't take the series names out. The script will do everything to process the names._**  
+[![overview_to_TOC.txt](https://github.com/gorbash1370/rename_packt_vids/misc/overview_html_to_TOC_text_small.png)](https://github.com/gorbash1370/rename_packt_vids/misc/overview_html_to_TOC_text_large.png)  
+
+It should look like this:  
+[![TOC.txt](https://github.com/gorbash1370/rename_packt_vids/misc/TOC_txt_small.PNG)](https://github.com/gorbash1370/rename_packt_vids/misc/TOC_txt_large_annot.PNG)
+
+3) Populate `path_TOC` with the path to the `TOC.txt` file you created via copy/pasting the headings from the `overview.html` supplied in the Packt course .zip folder.  
+![variable_strings](https://github.com/gorbash1370/rename_packt_vids/misc/paths_string_options)
+
+4) Update the series dictionary with the series / module names only (the final string).
+![Series Dictionary](https://github.com/gorbash1370/rename_packt_vids/misc/series_dictionary.png)
+
+5) Populate `path_folder` with the path to the folder containing the video files you want to rename.    
+6) Specify the `ext` variable with the file extension of the video files you want to rename (i.e. `.mp4`).  
+
+And it's good to go!
 
 
-# Limitations
+# Notes about the TOC.txt file
+[![overview.html Annotated](https://github.com/gorbash1370/rename_packt_vids/misc/series_episodes_annot_small.PNG)](https://github.com/gorbash1370/rename_packt_vids/misc/series_episodes_annot_large.PNG)  
 
+[![TOC.txt Annotated](https://github.com/gorbash1370/rename_packt_vids/misc/TOC_txt_small_annot.png)](https://github.com/gorbash1370/rename_packt_vids/misc/TOC_txt_large_annot.PNG)   
+
+It is important to understand that the transfer of new filename > video file is only partially intelligent. The script DOES pick out the series names and use these correctly for the `Sxx` part of the filename. And it does enforce strict alphanumeric ordering of the video file processing order (by adding a leading 0 before single digit numbers). 
+
+But, episodes just map in the order they appear in TOC.txt onto the strictly ascending alphanumeric order of the video files, 1:1. Therefore:
+- The simplest way to use this script is on a full course of videos: that way, the TOC episode list will exactly match the number of video files. 
+- If you supply a partial course, _starting at the beginning_, the script will handle this situation. This will mean the list of episodes extracted from the TOC is longer than the number of files: the script will just trim the number of episode entries to match the number of the videos.
+- However, if your batch of video files are NOT from the start of the course (i.e. you want to run renaming on episodes in series/module 4 and 5, but not on series 1, series 2, series 3), then you will have to remove the earlier series/episodes from TOC.txt.
+- * If the number of episodes from TOC.txt is less than the number of files, the script will throw an IndexError.
 
 # gorbash1370 Disclaimer
-This is an amateur project built mainly for coding practice, therefore...
-* Commentary may appear excessive (learning 'notes')
-* Some code is expanded (rather than shortened & simplified) for learning clarity.
-* I'm not a professional or trained Dev, so please always inspect code before running. Use at your own risk!
+This is an amateur project built mainly for coding practice, therefore please always inspect code before running. Use at your own risk!
 
 
 # Licence
-[Licence]()
+[Licence](https://github.com/gorbash1370/rename_packt_vids/LICENSE)
 
 
 # If you enjoy this project...
--- rename_packt_videos.py
+++ rename_packt_videos.py
@@ -1,31 +1,11 @@
-""" RENAME PACKT VIDEO FILES in format video1_1.mp4"""
+""" RENAME PACKT VIDEO FILES in format video1_1.mp4."""
 
-"""Limitations: 
-* 'Series' refers to a module in a full video course. S01 and E01 are used to maintain compatibility with whisper_wrapper.py transcription implementation.
-* This Script only works for this EXACT format of file names
-* No error handling etc.
-* TOC:
-* You can run this script on a full set of video files in a single course. You can also run it on an initial slice of the video files: but if you want to run it on a batch of video files which aren't at the START of the course (i.e. you want to run it on series/module 4 and 5, not 1-2-3, then you will have to modify the TOC.txt you supply to remove the earlier series/episodes. This is because the matching of file to new filename runs on a 1:1 basis. If the TOC
-* is longer than the number of files, then the script will cut the TOC to match the number of files. (no problems here)
-* If the TOC is shorter than the number of files, the script will throw an IndexError.
-* If you try to rename just later files (so Module/Series 4 and 5 but not 1-2-3, without modifying the TOC, then the script will rename your files incorrectly. There is no intelligent check to make sure the correct name is being given to the correct file: it is simply processed in strict alphabetical order.)
-* It is recommended to create a separate 'backup' folder and copy your originally named video files into it (packt always supply a zip, so they will also be in there if the zip isn't deleted after extraction) just incase there is an error with the renaming process. When the rename is complete, check the video filesizes in alphabetical order between the old and new folders to ensure the rename has happened in the correct order."""
-
-"""Naturally, for your own use, just fill in the strings for the target TOC path and video target path directly. They are imported from config for confidentiality for sharing on GitHub."""
-
-""" SETUP STEPS REQUIRED BEFORE RUNNING THIS SCRIPT:
-* Create a TOC 'Table of Contents' txt file by copying the Series & Episode names from overview.html that Packt supplies in the 'Package' folder. See README.md. # Delete only the top line "Table of Contents" from the .txt file # SCREENSHOT
-* Update path_TOC
-* Update series dictionary (just type in the series or module names)
-* Update target folder.
-* Looking at TOC, ensure that there aren't any IDENTICAL Episode and Series names: this will mess the entire thing up.
-
-"""
 import os
 import re
-from config import path_TOC, path_folder # don't need these if supplying path string directly in variables below
 
-""" ONLY MANUAL COMPLETION SECTION: Update Series Name only."""
+###################### MANUAL COMPLETION SECTION #############################
+
+""" Update Series names only """
 # Ensure the spelling and capitalisation exactly matches the Table of Contents
 series = {
     # Dictionary: standard Packt video filename, Series# and Series Name."""
@@ -36,13 +16,19 @@ series = {
     "video5": ["S05", "Troubleshooting Hardware and Networking"],
     }
 
-"Dictionary of Number Replacements"
-# Required to correctly order files with single digit numbers (otherwise 10 ends up before 2, etc)
-replacements = {"1": "01", "2": "02", "3": "03", "4": "04", "5": "05", "6": "06", "7": "07", "8": "08", "9": "09"}
+""" Specify target folder containing files to be renamed"""
+path_folder = "batch/" # path relative to script location
+
+""" Specify file extension to target"""
+ext = ".mp4" # NB: ALL the files in this folder with this file extension will be enumerated and processed!
+
+""" Specify the path to the TOC.txt (Table of Contents) file. """
+path_TOC = "batch/TOC.txt"
 
 
-""" Process the Table of Contents .txt file into a list of strings, with all extra characters and newlines removed"""
-path_TOC = r"D:\py_code_all\whisper_test\rename vids\Table of Contents_stripped.txt"
+##############################################################################
+
+""" Processes the Table of Contents .txt file into a list of strings, with all extra characters and newlines removed"""
 with open(path_TOC, "r") as file:
     TOC = file.read()
 TOCsplit = TOC.split("\n") # returns a list of strings, split at the newlines
@@ -55,7 +41,8 @@ TOCsplitstripped = [item for item in TOCsplitstripped if item != ''] # remove em
 print(f"{TOCsplitstripped}\n")
 # List of strings containing both the Series names and the Episode names mixed.
 
-""" Split the list into two lists: one with only the series names, and one with only the episode names"""
+
+""" Splits the list into two lists: one with only the series names, and one with only the episode names"""
 TOCseriesonly = [item for item in TOCsplitstripped if any(item == s[1] for s in series.values())] # add only the series names, excluding the episode names
 display_TOCseriesonly = "\n".join(TOCseriesonly)
 print(f"\nThe series names are:\n{display_TOCseriesonly}")
@@ -64,21 +51,15 @@ display_TOCepisodesonly = "\n".join(TOCepisodesonly)
 print(f"\nThe episode names are:\n{display_TOCepisodesonly}")
 
 
-"""Target folder"""
-path_folder = r"C:\path\to\your\folder\containingvids" # absolute path
-# OR
-path_folder = "batch/" # path relative to script location
-
-""".mp4 files"""
-
-ext = ".mp4" # NB: ALL the files in this folder with this file extension will be enumerated and processed!
-
+""" Reads the current video filenames into a sorted list """
 # Create list of current filenames of specified format in alphabetical order
 original_filenames = [f for f in os.listdir(path_folder) if f.endswith(ext)]
 sorted_filenames = sorted(original_filenames)
 display_sorted_filenames = "\n".join(sorted_filenames)
-print(f"\nIf episode numbers are more than single digit, the filenames will be displayed hire in an incorrectorder: {display_sorted_filenames}")
+print(f"\nIf episode numbers are more than single digit, the filenames will be displayed hire in an incorrect order: {display_sorted_filenames}")
 
+
+""" Renames the files to ensure that the episode number is surrounded by underscores on both sides """
 # Regex pattern to capture the digit before the file extension. This is in order to surround the full digit(s) with underscores either side (otherwise _12 will later be replaced by `01` and `02`)
 pattern = re.compile(r'(\d)(\{})$'.format(ext))
 
@@ -87,6 +68,7 @@ for videofile in sorted_filenames:
     # Replace match with the same digit as captured followed by an underscore
     new_name = pattern.sub(r'\g<1>_\g<2>', videofile)
     print(f"Filename after additional _: {new_name}")
+    # Rename the files on disc
     os.rename(os.path.join(path_folder, videofile), os.path.join(path_folder, new_name))
 
 # sorted_filenames needs recompiling as the files have been renamed
@@ -95,9 +77,16 @@ sorted_filenames = sorted(original_filenames)
 display_sorted_filenames = "\n".join(sorted_filenames)
 print(f"\n Filenames should now have an episode number bound by underscores on both sides: {display_sorted_filenames}")
 
-# Replace 
+
+"Dictionary for Number Replacements"
+# Required to correctly order files with single digit numbers (otherwise 10 ends up before 2, etc)
+replacements = {"1": "01", "2": "02", "3": "03", "4": "04", "5": "05", "6": "06", "7": "07", "8": "08", "9": "09"}
+
+
+""" For the new filename, replace: 
 # a) video1 with S01 {series name} 
-# b) the single digit episode numbers with a two digit representation (e.g. 1 with 01) so that the files are ordered correctly
+# b) the single digit episode numbers with a two digit representation (e.g. 1 with 01) so that the files are ordered correctly """
+
 for file in sorted_filenames:
     print(f"Filename before: {file}")
     new_name = file
@@ -108,15 +97,18 @@ for file in sorted_filenames:
         if f"_{old_num}_" in new_name:
             new_name = new_name.replace(f"_{old_num}_", f"_{new_num}_")
     print(f"Filename after: {new_name}")
+    # Rename the files on disc
     os.rename(os.path.join(path_folder, file), os.path.join(path_folder, new_name))
 
-# Sorted_filenames needs recompiling as the files have been renamed
+# sorted_filenames needs recompiling as the files have been renamed
 original_filenames = [f for f in os.listdir(path_folder) if f.endswith(ext)]
 sorted_filenames = sorted(original_filenames)
 display_sorted_filenames = "\n".join(sorted_filenames)
 print(f"\nThe filenames should now be in a strictly correct numerical order, (also with the Series name inserted). If they are not, then the files will end up with incorrect names: {display_sorted_filenames}")
 
-# Check if the number of files and the number of TOC entries match (i.e if ALL the video files for the full series are present). If not, cut the TOC length to match the files, to prevent an IndexError
+
+""" Checks if the number of files and the number of TOC entries match (i.e if ALL the video files for the full series are present). If not, cut the TOC length to match the files, to prevent an IndexError. See notes in README.md for full warning about TOC.txt entries which do not match the number of files."""
+
 num_files = len(sorted_filenames)
 print(f"Number of files: {num_files}")
 num_episodes = len(TOCepisodesonly)
@@ -127,12 +119,13 @@ if int(num_files) != int(num_episodes):
 else: 
     TOCepisodestouse = TOCepisodesonly
 
+""" Renames the files to include the episode name, writing to disc. """
 for index, episode_name in enumerate(TOCepisodestouse):
     print(f"Initial filename: {sorted_filenames[index]}")
-    # want to replace the _digit_ in the filename with " E{digit} {episode_name}" (file extension should remain)
+    # Replace the _digit_ in the filename with " E{digit} {episode_name}" (file extension remains)
     pattern = re.compile(r'_(\d+)_')
     episode_title = TOCepisodestouse[index]
     new_name = pattern.sub(r' E\g<1> {}'.format(episode_title), sorted_filenames[index])
     print(f"Final filename: {new_name}")
+    # Rename the files on disc
     os.rename(os.path.join(path_folder, sorted_filenames[index]), os.path.join(path_folder, new_name))
-    
